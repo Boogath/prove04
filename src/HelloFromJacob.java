8c5d00db99a393f6f7c678f90144a03a45c08d09
Arthur <cristechearturo@gmail.com>
2021-10-09 16:22:56 -0300

    Added a new print statement

--- src/HelloFromJacob.java
+++ src/HelloFromJacob.java
@@ -1,6 +1,11 @@
+
+
+
 public class HelloFromJacob {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jacob.");
+        System.out.println("");
+        System.out.println("Hey this is Arturo :D");
     }
 }
