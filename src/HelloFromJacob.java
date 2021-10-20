97c807d1d955c344d6a650d9060ec111b6fac4b8
echris10sen <chr15016@byui.edu>
2021-10-20 17:30:30 -0600

    hello

--- src/HelloFromJacob.java
+++ src/HelloFromJacob.java
@@ -1,6 +1,3 @@
-
-
-
 public class HelloFromJacob {
 
     public static void main(String[] args) {
@@ -8,6 +5,6 @@ public class HelloFromJacob {
         System.out.println("");
         System.out.println("Hey this is Arturo :D");
 
-        System.out.println("How are you Brother Emrich?");
+        System.out.println("Hello from Eric");
     }
 }
